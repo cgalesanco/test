ec7b721171f9895ca4bb9a25c8d6736961d33c97
Cesar Garcia <cgarciaalesanco@guidewire.com>
2013-05-13 14:01:05 +0100

    Start using AngularJS

---
+++ src/main/js/test-app.js
@@ -0,0 +1,18 @@
+angular.module('test-app',[]);
+
+function TestCtrl($scope) {
+  $scope.allCountries = [
+    { id:'US', name:'United States' },
+    { id:'ES', name:'Spain' }
+  ];
+  $scope.selectedCountries = [];
+
+  $scope.allStates = [
+    { id:'ES:LO', name:'La Rioja', country:$scope.allCountries[1]},
+    { id:'ES:M', name:'Madrid', country:$scope.allCountries[1]},
+    { id:'US:DE', name:'Delaware', country:$scope.allCountries[0]},
+    { id:'US:CA', name:'California', country:$scope.allCountries[0]}
+  ];
+  $scope.selectedStates = [];
+}
+TestCtrl.$inject = ['$scope'];
